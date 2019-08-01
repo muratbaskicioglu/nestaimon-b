c645813f09729179156763e69940fcd411190a11
Murat Baskıcıoğlu <murat.baskicioglu@getir.com>
2019-08-02 00:41:58 +0300

    Create auth rest endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards, Res, Req } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  googleLogin() {
+    // TODO: initiates the Google OAuth2 login flow
+  }
+
+  @Get('google/callback')
+  @UseGuards(AuthGuard('google'))
+  googleLoginCallback(@Req() req, @Res() res) {
+    const jwt: string = req.user.jwt;
+
+    if (jwt) {
+      res.redirect(`http://localhost:4200/login/success/${jwt}`);
+    } else {
+      res.redirect('http://localhost:4200/login/failure');
+    }
+  }
+}
