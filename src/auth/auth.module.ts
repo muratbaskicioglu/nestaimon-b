9464682912692ffaef0987928fb84c5b1d35f7e3
Murat Baskıcıoğlu <murat.baskicioglu@getir.com>
2019-08-02 01:05:18 +0300

    Fix providing config service and environment file path

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { GoogleStrategy } from './google.strategy';
+import { ConfigModule } from '../config/config.module';
 
 @Module({
+  imports: [ConfigModule],
   controllers: [AuthController],
   providers: [
     AuthService,
